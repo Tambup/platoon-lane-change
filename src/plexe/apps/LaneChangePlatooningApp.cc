2a5355322992f077f53f47aba4a4017e6bec9f87
stoppini <rove.albe@gmail.com>
2021-07-04 16:55:21 +0200

    Adding LaneChange and message

---
+++ src/plexe/apps/LaneChangePlatooningApp.cc
@@ -0,0 +1,109 @@
+//
+// Copyright (C) 2012-2021 Michele Segata <segata@ccs-labs.org>
+// Copyright (C) 2018-2021 Julian Heinovski <julian.heinovski@ccs-labs.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#include "plexe/apps/GeneralPlatooningApp.h"
+#include "plexe/protocols/BaseProtocol.h"
+#include "plexe/apps/LaneChangePlatooningApp.h"
+#include "veins/modules/mac/ieee80211p/Mac1609_4.h"
+
+
+using namespace veins;
+
+namespace plexe {
+
+Define_Module(LaneChangePlatooningApp);
+
+void LaneChangePlatooningApp::initialize(int stage)
+{
+    GeneralPlatooningApp::initialize(stage);
+
+    if (stage == 1) {
+        // connect maneuver application to protocol
+        protocol->registerApplication(MANEUVER_TYPE, gate("lowerLayerIn"), gate("lowerLayerOut"), gate("lowerControlIn"), gate("lowerControlOut"));
+        // register to the signal indicating failed unicast transmissions
+        findHost()->subscribe(Mac1609_4::sigRetriesExceeded, this);
+
+        std::string laneChangeManeuverName = par("laneChange").stdstringValue();
+        if (laneChangeManeuverName == "LaneChange")
+            laneChangeManeuver = new LaneChange(this);
+        else
+            throw new cRuntimeError("Invalid join maneuver implementation chosen");
+
+        scenario = FindModule<BaseScenario*>::findSubModule(getParentModule());
+    }
+}
+
+void LaneChangePlatooningApp::handleSelfMsg(cMessage* msg)
+{
+    if (laneChangeManeuver && laneChangeManeuver->handleSelfMsg(msg)) return;
+    BaseApp::handleSelfMsg(msg);
+}
+
+void LaneChangePlatooningApp::onPlatoonBeacon(const PlatooningBeacon* pb)
+{
+    laneChangeManeuver->onPlatoonBeacon(pb);
+    // maintain platoon
+    BaseApp::onPlatoonBeacon(pb);
+}
+
+void LaneChangePlatooningApp::onManeuverMessage(ManeuverMessage* mm)
+{
+    if (activeManeuver) {
+        activeManeuver->onManeuverMessage(mm);
+    }
+    else {
+        laneChangeManeuver->onManeuverMessage(mm);
+    }
+    delete mm;
+}
+
+
+void LaneChangePlatooningApp::startChangeLaneManeuver(int platoonId, int leaderId)
+{
+    ASSERT(getPlatoonRole() == PlatoonRole::NONE);
+    ASSERT(!isInManeuver());
+
+    LaneChangeParameters params;
+    params.platoonId = platoonId;
+    params.leaderId = leaderId;
+    laneChangeManeuver->startManeuver(&params);
+}
+
+
+
+void LaneChangePlatooningApp::receiveSignal(cComponent* src, simsignal_t id, cObject* value, cObject* details)
+{
+    if (id == Mac1609_4::sigRetriesExceeded) {
+        BaseFrame1609_4* frame = check_and_cast<BaseFrame1609_4*>(value);
+        ManeuverMessage* mm = check_and_cast<ManeuverMessage*>(frame->getEncapsulatedPacket());
+        if (frame) {
+            laneChangeManeuver->onFailedTransmissionAttempt(mm);
+        }
+    }
+}
+
+
+LaneChangePlatooningApp::~LaneChangePlatooningApp()
+{
+    delete laneChangeManeuver;
+}
+
+} // namespace plexe
---
+++ src/plexe/apps/LaneChangePlatooningApp.h
@@ -0,0 +1,111 @@
+
+//
+// Copyright (C) 2012-2021 Michele Segata <segata@ccs-labs.org>
+// Copyright (C) 2018-2021 Julian Heinovski <julian.heinovski@ccs-labs.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef LANECHANGEPLATTONINGAPP_H_
+#define LANECHANGEPLATTONINGAPP_H_
+
+#include <algorithm>
+#include <memory>
+
+#include "plexe/apps/BaseApp.h"
+#include "plexe/apps/GeneralPlatooningApp.h"
+#include "plexe/maneuver/Maneuver.h"
+#include "plexe/maneuver/LaneChange.h"
+
+
+#include "plexe/messages/ManeuverMessage_m.h"
+#include "plexe/messages/UpdatePlatoonData_m.h"
+
+#include "plexe/scenarios/BaseScenario.h"
+
+#include "veins/modules/mobility/traci/TraCIConstants.h"
+#include "veins/modules/utility/SignalManager.h"
+
+namespace plexe {
+
+/**
+ * General purpose application for Platoons
+ *
+ * This class handles maintenance of an existing Platoon by storing relevant
+ * information about it and feeding the CACC.
+ * It also provides an easily extendable base for supporting arbitrary
+ * maneuvers.
+ * Currently, it already contains functionality for the join maneuver.
+ *
+ * @see BaseApp
+ * @see Maneuver
+ * @see JoinManeuver
+ * @see JoinAtBack
+ * @see ManeuverMessage
+ */
+class LaneChangePlatooningApp : public GeneralPlatooningApp {
+
+public:
+    /** c'tor for LaneChangePlatooningApp */
+    LaneChangePlatooningApp()
+        : GeneralPlatooningApp()
+        , laneChangeManeuver(nullptr)
+
+    {
+    }
+
+    /** d'tor for LaneChangePlatooningApp */
+    virtual ~LaneChangePlatooningApp();
+
+    virtual void startChangeLaneManeuver(int platoonId, int leaderId);
+
+    /** override from GeneralPlatooningApp */
+    virtual void initialize(int stage) override;
+
+    /** override from GeneralPlatooningApp */
+    virtual void handleSelfMsg(cMessage* msg) override;
+
+
+
+protected:
+    /** used to receive the "retries exceeded" signal **/
+    virtual void receiveSignal(cComponent* src, simsignal_t id, cObject* value, cObject* details) override;
+
+    /**
+     * Handles PlatoonBeacons
+     *
+     * @param PlatooningBeacon pb to handle
+     */
+    virtual void onPlatoonBeacon(const PlatooningBeacon* pb) override;
+
+    /**
+     * Handles ManeuverMessages
+     *
+     * @param ManeuverMessage mm to handle
+     */
+    virtual void onManeuverMessage(ManeuverMessage* mm);
+
+
+
+private:
+    /** platoons change lane implementation */
+    Maneuver* laneChangeManeuver;
+};
+
+} // namespace plexe
+
+#endif
